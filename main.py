c9d1aea0b5fe9c573ea8e24ecf4fdbcb69873ab3
Nick <49598942+StickOnAStick@users.noreply.github.com>
2023-05-18 16:52:10 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from machine import UART, Pin
+
+uart = UART(16, baudrate=9600)
+HC05 = Pin(14, Pin.OUT)
+HC05.value(1) #Leave bluetooth on
+ESC = Pin(34, Pin.OUT)
+
+while True:
+    if uart.any():
+        data_received = str(uart.readline())
+        if 'Throttle On' in data_received:
+            ESC.on()
+    else:
+        ESC.off()
+     
+    
+    
